671ed3fc638588f2050baa653d18100366a6a49b
Rakeshkuruba53 <kurubarakesh2001@gmail.com>
2024-02-08 16:28:20 +0530

    implemented the messaging services

---
+++ src/test/java/com/jsp/sap/MeeshoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsp.sap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeeshoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
